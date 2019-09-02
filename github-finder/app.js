801147d76d5695cf722a941c2a9285fa38c8135c
alexeispirit <alexeispirit@gmail.com>
2019-09-02 16:57:23 +0500

    GitHub Finder [Fetching Profile Data]

---
+++ github-finder/app.js
@@ -0,0 +1,24 @@
+// Init
+const github = new GitHub();
+
+// Search input
+const searchUser = document.getElementById("searchUser");
+
+// Search input event listener
+searchUser.addEventListener("keyup", e => {
+  // Get input text
+  const userText = e.target.value;
+
+  if (userText !== "") {
+    // make http call
+    github.getUser(userText).then(data => {
+      if (data.profile.message === "Not Found") {
+        // show alert
+      } else {
+        // show profile
+      }
+    });
+  } else {
+    // clear profile
+  }
+});
