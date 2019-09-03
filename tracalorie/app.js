a313f8529ebbc697acfd52d51a6c33ce4441c768
alexeispirit <alexeispirit@gmail.com>
2019-09-03 17:58:10 +0500

    Tracalorie [Delete and Clear from Local Storage]

--- tracalorie/app.js
+++ tracalorie/app.js
@@ -23,6 +23,23 @@ const StorageCtrl = (function() {
         items = JSON.parse(localStorage.getItem("items"));
       }
       return items;
+    },
+    updateItemStorage: function(updatedItem) {
+      let items = JSON.parse(localStorage.getItem("items"));
+      items.forEach((item, index) => {
+        if (updatedItem.id === item.id) {
+          items.splice(index, 1, updatedItem);
+        }
+      });
+      localStorage.setItem("items", JSON.stringify(items));
+    },
+    deleteItemFromStorage: function(id) {
+      let items = JSON.parse(localStorage.getItem("items"));
+      items = items.filter(item => item.id !== id);
+      localStorage.setItem("items", JSON.stringify(items));
+    },
+    clearItemsFromStorage: function() {
+      localStorage.removeItem("items");
     }
   };
 })();
@@ -346,6 +363,9 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl) {
     // add total calories to UI
     UICtrl.showTotalCalories(totalCalories);
 
+    // update local storage
+    StorageCtrl.updateItemStorage(updatedItem);
+
     // clear edit state
     UICtrl.clearEditState();
 
@@ -368,6 +388,9 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl) {
     // add total calories to UI
     UICtrl.showTotalCalories(totalCalories);
 
+    // Delete from local storage
+    StorageCtrl.deleteItemFromStorage(currentItem.id);
+
     // clear edit state
     UICtrl.clearEditState();
 
@@ -384,6 +407,8 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl) {
     UICtrl.showTotalCalories(totalCalories);
     // Clear ui list
     UICtrl.removeItems();
+    // clear from localStorage
+    StorageCtrl.clearItemsFromStorage();
     // hide ul
     UICtrl.hideList();
   };
